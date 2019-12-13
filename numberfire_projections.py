7576e7d5c1fa22f0cefd0f8cdb309f66f4db4a11
Ford Higgins <fordhiggins@Fords-MacBook-Pro.local>
2019-12-12 22:39:14 -0800

    modularize data scraping

--- numberfire_projections.py
+++ numberfire_projections.py
@@ -32,16 +32,46 @@ class nf_projections:
     # in a flask app? or just CLI?
     # how far does your projection need to outpace the other side in a X for 1 trade
     # where X > 1
+    pure_positions = ["QB", "RB", "WR", "TE"]
+    position_types = ["QB", "RB", "WR", "TE", "flex", "all"]
 
-    def __init__(self, scoring_system, position=None):
+    def __init__(self, scoring_system):
         self.scoring_system = self._get_scoring_map(scoring_system)
-        self.position = position #make these property functions?
+        # self.position = position #make these property functions?
         self.data = list()
         self.projections = dict()
+        self.base_url = "https://www.numberfire.com/nfl/fantasy/remaining-projections/"
 
-    def get_data(self, url):
-        # make this the default?
-        # https://www.numberfire.com/nfl/fantasy/remaining-projections/
+    def make_url(self, position):
+        if position == "RB/WR":
+            return self.base_url + "rbwr"
+        return self.base_url + position.lower()
+
+    def get_data(self, position):
+        url = self.make_url(position)
+        raw_data = self.scrape_data(url)
+
+        player_header = self.get_player_header(raw_data)
+        player_names = self.get_player_names(raw_data)
+        player_projections = self.get_player_projections(raw_data)
+        projection_headers = self.get_projection_headers(raw_data)
+
+        # data_list = list()
+        # for idx, player in enumerate(player_names):
+        #     player_dict = dict(zip(projection_headers, player_projections[idx]))
+        #     player_dict[player_header] = player
+        #     self.data.append(player_dict)
+
+        self.data = [
+            dict(
+                zip(projection_headers, player_projections[idx]),
+                **{player_header, player}
+            )
+            for idx, player in enumerate(player_names)
+        ]
+
+    @staticmethod
+    def scrape_data(url):
         r = requests.get(url)
         soup = BeautifulSoup(r.content, "html.parser")
         raw_data = (
@@ -52,33 +82,39 @@ class nf_projections:
             )
         )
 
-        player_header = (
-            raw_data.find("table").find("thead").find_all("tr")[1].get_text().strip()
-        )
-        player_names = [
+        return raw_data
+
+    @staticmethod
+    def get_player_header(data):
+        return data.find("table").find("thead").find_all("tr")[1].get_text().strip()
+
+    @staticmethod
+    def get_player_names(data):
+        return [
             td.find("span", {"class": "full"}).get_text()
-            for td in raw_data.find("table").find("tbody").find_all("td")
+            for td in data.find("table").find("tbody").find_all("td")
         ]
-        player_projections = [
+
+    @staticmethod
+    def get_player_projections(data):
+        return [
             [td.get_text().strip() for td in tr.find_all("td")]
-            for tr in raw_data.find_all("table")[1].find("tbody").find_all("tr")
+            for tr in data.find_all("table")[1].find("tbody").find_all("tr")
         ]
-        projection_headers = [
+
+    @staticmethod
+    def get_projection_headers(data):
+        return [
             th["title"]
-            for th in raw_data.find_all("table")[1]
+            for th in data.find_all("table")[1]
             .find("thead")
             .find_all("tr")[1]
             .find_all("th")
         ]
 
-        # data_list = list()
-        for idx, player in enumerate(player_names):
-            player_dict = dict(zip(projection_headers, player_projections[idx]))
-            player_dict[player_header] = player
-            self.data.append(player_dict)
-
     def convert_projections(self):
-
+        ## how to deal with non-QB scoring? try to avoid deep indentation
+        ## modularize the scoring calculations and turn this into a list comp?
         for row in self.data:
             player = row["Player"]
             proj_points = (
@@ -97,42 +133,3 @@ class nf_projections:
         if scoring_system not in self.scoring_map:
             raise Exception("System not in scoring map")
         return self.scoring_map[scoring_system]
-
-
-"""
-table = soup.find('main').find('section', {'class':"grid__two-col--main grid--1-2"}).find('div', {'class': 'projection-wrap projection-rankings power-rankings'})
-player_header = table.find('table').find('thead').find_all('tr')[1].get_text().strip()
-player_names = [td.find('span', {'class':'full'}).get_text() for td in table.find('table').find('tbody').find_all('td')]
-player_projections = [[td.get_text().strip() for td in tr.find_all('td')] for tr in table.find_all('table')[1].find('tbody').find_all('tr')]
-projection_headers = [th['title'] for th in table.find_all('table')[1].find('thead').find_all('tr')[1].find_all('th')]
-
-# insert the player name into 0 index for player projection
-
-"""
-
-"""
-CBS:
-cbs_qb_url = 'https://www.cbssports.com/fantasy/football/stats/QB/2019/restofseason/projections/nonppr/'
-cbs_qb_r = requests.get(cbs_qb_url)
-cbs_qb_soup = BeautifulSoup(cbs_qb_r.content, 'html.parser')
-cq_table = cbs_qb_soup.find('table', {'class': 'TableBase-table'})
-
-# repeat for each player and figure out how to save
-cbs_test_player = [tr.find_all('td') for tr in cq_table.find('tbody').find_all('tr')][1]
-
-for i, elem in enumerate(cbs_test_player):
-	if i==0:
-		p_name = elem.find_all('a')[1].get_text()
-		p_pos = elem.find('span', {'class':'CellPlayerName-position'}).get_text().strip()
-	else:
-		elem.get_text().strip()
-
-cbs_headers = cq_table.find('thead').find_all('tr')[1].find_all('th')
-for i, elem in enumerate(cbs_headers):
-	if i==0:
-		elem.find('a').get_text()
-	else:
-		elem.find('div', {'class':'Tablebase-tooltipInner'}).get_text().strip()
-
-# get ytd stats by replacing 'restofseason/projections' with 'ytd/stats'
-"""
