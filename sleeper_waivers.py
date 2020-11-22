2ec11291911b9613f6a72625d040c89a70e434b6
Ford Higgins <wfordh@users.noreply.github.com>
2020-11-22 13:47:56 -0500

    save kept players

--- sleeper_waivers.py
+++ sleeper_waivers.py
@@ -8,11 +8,16 @@ from tqdm import tqdm, trange
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--dry_run", type=bool, required=True)
+parser.add_argument("-p", "--positions", required=False)
 
 def main():
 	league_id = os.environ.get("SLEEPER_LEAGUE_ID", "")
 	user_id = os.environ.get("SLEEPER_USER_ID", "")
-	is_dry_run = False
+	
+	args = parser.parse_args()
+	command_args = dict(vars(args))
+	is_dry_run = command_args.pop("dry_run", None)
+	keep_positions = ("QB", "RB", "WR", "TE") # make CLI arg. D/ST??
 
 	league = League(league_id)
 	players = Players()
@@ -27,6 +32,13 @@ def main():
 			all_players = json.load(infile)
 
 	own_team = [team for team in league_rosters if team["owner_id"]==user_id].pop()
+	keep_players = {p_id: p_data for p_id, p_data in all_players.items() if p_data["position"] in keep_positions}
+	# save keep_players for testing
+	with open("./data/sleeper_players_keep.json", "w") as outfile:
+		json.dump(keep_players, outfile)
+	# ID free agents by comparing keep_players to rosters
+	# pull projections
+	# add projections in to rosters for comparisons
 
 
 
