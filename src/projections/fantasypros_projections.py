8b3d6c79edfd420a5d00c38ede0794bc17768446
Ford Higgins <wfordh@users.noreply.github.com>
2021-11-11 11:35:04 -0800

    more work on fantasypros projections

--- src/projections/fantasypros_projections.py
+++ src/projections/fantasypros_projections.py
@@ -1,13 +1,19 @@
 import requests
+import chompjs
 from bs4 import BeautifulSoup
 
 class fantasyprosProjctions:
     
+    pure_positions = ["QB", "RB", "WR", "TE"]
+
     def __init__(self, scoring_system):
-        self.scoring_system = self._get_scoringn_map(scoring_system)
+        self.scoring_system = self._get_scoring_map(scoring_system)
         self.data = list()
-        self.projectionsn = dict()
+        self.projections = dict()
         self.base_url = "https://fantasypros.com/nfl/rankings"
+
+   def construct_url(self, position):
+        return f"{self.base_url}/ros-{self.scoring_system}-{position.lower()}.php"
 
     def get_data(self, soup, position):
         scripts = soup.find_all("script")
@@ -20,8 +26,36 @@ class fantasyprosProjctions:
         # pull out only necessary things, returning the raw data for now 
         return player_dict
 
-    def compile_data(self, position):
+    def compile_data(self, positions):
+        if positions == "flex":
+            positions = ["RB", "WR", "TE"]
+        elif positions == "all":
+            positions = ["QB", "RB", "WR", "TE"]
+
+ 
         for position in positions:
-            resp = requests.get(f"{self.base_url}/ros-{self.scoring_sytem}-{position.lower()}.php" 
+            resp = requests.get(self.construct_url(position))
             soup = BeautifulSoup(resp.content, 'html.parser')
             self.data.extend(self.get_data(soup, position))
+
+    def save_projections(self, file_path):
+        data_folder = Path.cwd() / "data"
+        full_path = data_folder / file_path
+        with open(full_path, "w") as outfile:
+            field_names = self.data[0].keys()
+            dict_writer = csv.DictWriter(outfile, field_names)
+            dict_writer.writeheader()
+            dict_writer.writerows(self.data)
+
+    def load_projections(self, file_path):
+        data_folder = Path.cwd() / "data"
+        full_path = data_folder / file_path
+        with open(full_path, "r") as infile:
+            reader = csv.DictReader(infile)
+            for row in reader:
+                self.data.append(row)
+
+    def _get_scoring_map(self, scoring_system):
+        if scoring_system not in self.scoring_map:
+            raise Exception("System not in scoring map")
+        return self.scoring_map[scoring_system]
